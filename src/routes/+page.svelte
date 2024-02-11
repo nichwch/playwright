3d7c5ad7ccac9d40d66f7afe5c1a43d9d763c534
nichwch <nichwch@gmail.com>
2024-02-11 18:40:04 -0500

    basic skeleton of project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,20 +6,24 @@
 	let names = ['nick'];
 </script>
 
-<div class="flex h-full w-full">
+<div class=" box-border flex h-screen w-full overflow-y-hidden p-10">
 	<!-- sidebar -->
-	<div class="h-full basis-1/4 border-r border-r-black"></div>
+	<div class=" h-full basis-1/4 border-r border-r-black"></div>
 	<!-- main content -->
-	<div class="h-full basis-3/4">
-		<Editor
-			bind:content
-			keywordMap={{
-				'@': {
-					component: MentionComponent,
-					recognized: names,
-					allowUnrecognized: false
-				}
-			}}
-		/>
+	<div class=" flex h-full basis-3/4 flex-col p-10">
+		<!-- editor -->
+		<div class="basis-1/2">
+			<Editor
+				bind:content
+				keywordMap={{
+					'@': {
+						component: MentionComponent,
+						recognized: names,
+						allowUnrecognized: false
+					}
+				}}
+			/>
+		</div>
+		<div class="basis-1/2 border border-black">hello</div>
 	</div>
 </div>
